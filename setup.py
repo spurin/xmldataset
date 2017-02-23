88441753544ff705517064bc2c10683ea83acb6d
James Spurin <james@spurin.com>
2017-02-23 21:35:39 +0000

    updated version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='xmldataset',
-    version='1.0.0',
+    version='1.0.1',
     description='XML Dataset: xml parsing made easy',
     long_description=readme + '\n\n' + history,
     author='James Spurin',
